bb844541344a67aa2316b2248edd6060d45cba99
jcforero2 <jc.forero2@uniandes.edu.co>
2025-11-19 08:13:20 -0500

    Punto 1

---
+++ src/app/emprendedor/emprendedor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Emprendedor } from './emprendedor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmprendedorService {
+  private apiUrl = 'https://raw.githubusercontent.com/k-garces/ISIS2603_202520_S4_P2_Practica/refs/heads/main/json';
+
+  constructor(private http: HttpClient) {}
+  getEmprendedores(): Observable<Emprendedor[]> {
+    return this.http.get<Emprendedor[]>(`${this.apiUrl}/emprendedores.json`);
+  }
+  getEmprendedorDetail(id: number): Observable<Emprendedor> {
+    return this.http.get<Emprendedor>(`${this.apiUrl}/${id}/emprendedor.json`);
+  }
+}
