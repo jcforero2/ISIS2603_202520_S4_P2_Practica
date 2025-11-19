95ca05aeb8157c7a6b9a20aed0e2c65fe82af5a8
jcforero2 <jc.forero2@uniandes.edu.co>
2025-11-19 08:51:42 -0500

    Eliminacion de datos quemados

--- src/app/emprendedor/emprendedor.service.ts
+++ src/app/emprendedor/emprendedor.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Emprendedor } from './emprendedor';
+import { EmprendedorDetail } from './emprendedor-detail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmprendedorService {
+
   private apiUrl = 'https://raw.githubusercontent.com/k-garces/ISIS2603_202520_S4_P2_Practica/refs/heads/main/json';
 
   constructor(private http: HttpClient) {}
+
   getEmprendedores(): Observable<Emprendedor[]> {
     return this.http.get<Emprendedor[]>(`${this.apiUrl}/emprendedores.json`);
   }
-  getEmprendedorDetail(id: number): Observable<Emprendedor> {
-    return this.http.get<Emprendedor>(`${this.apiUrl}/${id}/emprendedor.json`);
+
+  getEmprendedorDetail(id: number): Observable<EmprendedorDetail> {
+    return this.http.get<EmprendedorDetail>(`${this.apiUrl}/emprendedor-detail-${id}.json`);
   }
 }
